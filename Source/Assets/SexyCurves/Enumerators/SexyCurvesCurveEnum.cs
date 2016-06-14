d979c2d98a5662174b1992ad1a7d0cf77dcf93ab
Konstantin <spamschrank@googlemail.com>
2016-06-14 10:58:24 +0200

    [ADDED] File Header Comments

--- Source/Assets/SexyCurves/Enumerators/SexyCurvesCurveEnum.cs
+++ Source/Assets/SexyCurves/Enumerators/SexyCurvesCurveEnum.cs
@@ -1,4 +1,12 @@
-﻿namespace SexyCurves.Enumerators
+﻿// SexyCurvesCurveEnum.cs - SexyCurves
+// 
+// Created at 15:39, on 03.06.2016
+// 
+// By Konstantin Rudolph
+// 
+// Last modified at 10:54, on 14.06.2016
+
+namespace SexyCurves.Enumerators
 {
     /// <summary>
     ///     Describes which axis the curve(s) which shall be modified is describing.
--- Source/Assets/SexyCurves/Enumerators/SexyCurvesMainModuleEnum.cs
+++ Source/Assets/SexyCurves/Enumerators/SexyCurvesMainModuleEnum.cs
@@ -1,4 +1,12 @@
-﻿namespace SexyCurves.Enumerators
+﻿// SexyCurvesMainModuleEnum.cs - SexyCurves
+// 
+// Created at 15:39, on 03.06.2016
+// 
+// By Konstantin Rudolph
+// 
+// Last modified at 10:54, on 14.06.2016
+
+namespace SexyCurves.Enumerators
 {
     /// <summary>
     ///     Represents curves in the main module.
